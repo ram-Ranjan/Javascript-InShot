4481ef0744b99117cc7aaf4487f5cba5e3a8288d
Ranjan Kumar <127039261+ram-Ranjan@users.noreply.github.com>
2024-02-18 13:39:34 +0000

    Datatypes completed

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,21 @@
+
+"use strict"  //treat all js code as they are of latest versions-(now a days is present implicitely)
+
+//alert(4*10) cant be used in node-works only with browser which acts as a document
+
+console.log(33  
+    + 
+    55)//code readability should be preferred
+
+
+     var num ='five'
+    //let x=typeof(num)
+    //console.log(x)
+    console.log(typeof(num))
+
+    let x
+    console.log(typeof(null))
+
+    console.log(typeof(undefined))
+
+    console.log(typeof(x))
